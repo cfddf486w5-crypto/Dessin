4229b832de97891fe58d8d161b959a4db725fc47
cfddf486w5-crypto <cfddf486w5@privaterelay.appleid.com>
2026-03-05 00:20:53 -0500

    Add dedicated Map3D page with local offline editor and persistence

---
+++ js/lib/OrbitControls.js
@@ -0,0 +1,3 @@
+/* Local controls placeholder: interactions are implemented directly in js/map3d.js for this app. */
+window.THREE = window.THREE || {};
+window.THREE.OrbitControls = function OrbitControls() {};
